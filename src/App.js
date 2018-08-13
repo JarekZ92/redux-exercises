e7b46ffb1dce7c30f57cbfb4ddb436f8e40030c0
Jarek <z.jarek92@gmail.com>
2018-08-13 14:13:14 +0200

    added missing imports

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import Counter from './Counter'
 import CounterButtons from './Counter/CounterButtons'
+import counterReducer from './Counter/Store'
+
+const reducers = combineReducers({
+  counter: counterReducer
+})
+
+const store = createStore(reducers)
 
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Counter />
-        <CounterButtons />
-      </div>
+      <Provider store={store}>
+        <div>
+          <Counter />
+          <CounterButtons />
+        </div>
+      </Provider >
     );
   }
 }
