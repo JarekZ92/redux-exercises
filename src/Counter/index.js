fcb5ae621cb155c98d44f1575052603d9dd4470e
Jarek <z.jarek92@gmail.com>
2018-08-13 11:14:44 +0200

    added onClick functions and pased them into buttons

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
 
 class Counter extends React.Component {
+    state = {
+        value: 7
+    }
+
+    addHandler = () => {
+        this.setState({
+            value: 2
+        })
+    }
+
+    subHandler = () => {
+        this.setState({
+            value: 3
+        })
+    }
+
+
+
+
     render() {
         return (
             <div>
-                My awesome counter: {this.props.value}
+                My awesome counter: {this.state.value}
                 <div>
-                    <button>Add</button>
-                    <button>Sub</button>
+                    <button onClick={this.addHandler}>Add</button>
+                    <button onClick={this.subHandler}>Sub</button>
                 </div>
             </div>
         )
